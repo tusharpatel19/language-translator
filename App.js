243e3490fc0306e571c236333e299999003bd3b6
tusharpatel19 <148246678+tusharpatel19@users.noreply.github.com>
2025-06-05 10:20:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// App.js
+
+import './App.css';
+import Translator from './components/Translator';
+
+function App() {
+  return (
+    <div className="App">
+      <Translator />
+    </div>
+  );
+}
+
+export default App;
